bd408e1cf5cd28b88d84b742b94cd36f06f3ddce
Ibrahim Hz <ibrahimdmc666@gmail.com>
2023-03-29 14:57:07 +0530

    Initial Structure of DAM

---
+++ client/src/components/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./homePage.css";
+
+import Navbar from '../../Navbar/Navbar';
+
+const HomePage = () => {
+  return (
+    <>
+        <Navbar />
+    </>    
+  )
+}
+
+export default HomePage
